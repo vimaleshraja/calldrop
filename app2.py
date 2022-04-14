f4d1244cde4558ba1ab45fe9ef13a24624f73aac
vimaleshraja <54736154+vimaleshraja@users.noreply.github.com>
2022-04-14 13:27:21 +0530

    new

--- app2.py
+++ app2.py
@@ -179,7 +179,6 @@ else:
 		fig, ax = plt.subplots(figsize=(8, 5))
 		ax.plot(a['Original_Value'],marker='.', linestyle='-', linewidth=0.5, label='Original')
 		ax.plot(a['Prediction'],marker='o', linestyle='-', linewidth=0.5, label= 'Prediction')
-		ax.legend(bbox_to_anchor=(1,0), loc="lower right", 
-                bbox_transform=fig.transFigure, ncol=3)
+		ax.legend(bbox_to_anchor=(1,0), loc="lower right",bbox_transform=fig.transFigure, ncol=3)
 		st.plotly_chart(fig)
 
